8cb2654fb14b5e87b2e25c706248dfef1083759a
y2onsoo <abbcaog000@naver.com>
2023-04-13 10:17:47 +0900

    작업중

---
+++ js/sub-1.js
@@ -0,0 +1,45 @@
+/////////////top-pop
+$(document).ready(function () {
+  $('#pop-btn').on('click',function(){
+    
+  if($('#pop-btn').hasClass('off')){
+    $('.top-pop').css('height','50px');
+    $('.pop-text').removeClass('d-none');
+    $('#pop-btn').removeClass('off');
+    $('#pop-btn').addClass('on');
+    $('#pop-btn').removeClass('xi-angle-down');
+    $('#pop-btn').addClass('xi-angle-up');
+}else{
+    $('.top-pop').css('height','20px');
+    $('.pop-text').addClass('d-none');
+    $('#pop-btn').removeClass('on');
+    $('#pop-btn').addClass('off');
+    $('#pop-btn').removeClass('xi-angle-up');
+    $('#pop-btn').addClass('xi-angle-down');
+}
+  });
+});
+
+
+/////스크롤 따라다니는 헤더
+onscroll = function() {
+  var nVScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  if(nVScroll > 100){
+    $("#header").addClass('fixed');
+    $("#header").removeClass('absol');
+  } 
+  else{ 
+    $("#header").removeClass('fixed');
+    $("#header").addClass('absol');
+}
+};
+
+////////////슬라이드 1번째
+var swiper1 = new Swiper(".first-slide", {
+  slidesPerView: 7,
+  spaceBetween: 50,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
